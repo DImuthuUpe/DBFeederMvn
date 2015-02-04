51f1aa278d3d062d0084a914383f64cb2d1a6154
Dimuthu Upeksha <dimuthu.upeksha2@gmail.com>
2015-02-04 11:11:53 +0530

    Minor commit

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,5 +5,7 @@ echo "Cloning corpus.sinhala.tools"
 git clone https://github.com/madurangasiriwardena/corpus.sinhala.tools
 cd corpus.sinhala.tools
 echo "Building corpus tools"
-pwd
 mvn clean install
+echo "Adding Oracle Dependencies"
+cp ../src/main/resources/ojdbc7.jar .
+mvn install:install-file -Dfile=ojdbc7.jar -DgroupId=ojdbc -DartifactId=ojdbc -Dversion=7 -Dpackaging=jar
